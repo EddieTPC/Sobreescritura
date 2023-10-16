e0fb7023bb18d1cb998544bcae6168e7d74c7063
EddieTPC <147268407+EddieTPC@users.noreply.github.com>
2023-10-15 22:24:13 -0700

    Update Instrumento.java

--- Instrumento.java
+++ Instrumento.java
@@ -8,7 +8,9 @@ public class Instrumento {
     }
         
     static class guitarra extends Instrumento{
-        //La anotación @Override simplemente se utiliza, para forzar al compilador a comprobar en tiempo de compilación que estás sobrescribiendo correctamente un método, y de este modo evitar errores en tiempo de ejecución, los cuales serían mucho más difíciles de detectar.
+        /*La anotación @Override simplemente se utiliza, para forzar al compilador a 
+        comprobar en tiempo de compilación que estás sobrescribiendo correctamente un método,
+        y de este modo evitar errores en tiempo de ejecución, los cuales serían mucho más difíciles de detectar. */
         @Override         
         public void tocarInstrumento() {
         System.out.println("Tocar La Guitarra");
